1717411a447b3022f05303166d9fd965783b2cc1
Matheus Leite <matheusssleite@outlook.com>
2024-11-11 15:20:20 -0300

    Implementando lógica de filtro nos projetos

--- js/script.js
+++ js/script.js
@@ -35,7 +35,43 @@ $(document).ready(function() {
     // Parallax
     setTimeout(function() {
         $("#data-area").parallax({imageSrc: "img/cidadeparallax.png"})
+        $("#apply-area").parallax({imageSrc: "img/cidadeparallax.png"})
     }, 250);
+
+    // Filtro do Portfólio
+
+    $(".filter-btn").on("click", function() {
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+        $(".main-btn").removeClass("active");
+        $(this).addClass("active");
+
+        if (type == "dsg-btn"){
+            eachBoxes('dsg', boxes);
+        } else if (type == "dev-btn") {
+            eachBoxes("dev", boxes);
+        } else if (type == "seo-btn") {
+            eachBoxes("seo", boxes);
+        } else {
+            eachBoxes("all", boxes);
+        }
+    });
+
+    function eachBoxes(type, boxes) {
+        type = type.replace("btn-", "");
+        if (type == "all"){
+            $(boxes).fadeIn();
+        } else {
+            $(boxes).each(function() {
+                if (!$(this).hasClass(type)){
+                    $(this).fadeOut("slow");
+                } else {
+                    $(this).fadeIn();
+                }
+            })
+        }
+    }
 
 
 });
